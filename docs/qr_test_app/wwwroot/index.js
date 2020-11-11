f120fd120f1ec04b7b5e18adf5249b59e5bbfd36
thien-vu <thien-vu@rvsc.ryomo-gr.com>
2020-11-11 15:05:09 +0700

    add qr_test_app

---
+++ docs/qr_test_app/wwwroot/index.js
@@ -0,0 +1,78 @@
+﻿window.startQrReading = () => {
+
+    var btnScan = document.getElementById("btnScan");
+
+    var video = document.createElement("video");
+    var canvasElement = document.getElementById("canvas");
+    var canvas = canvasElement.getContext("2d");
+    var loadingMessage = document.getElementById("loadingMessage");
+    var outputContainer = document.getElementById("output");
+    var outputMessage = document.getElementById("outputMessage");
+    var outputData = document.getElementById("outputData");
+
+    var frame;
+
+    function drawLine(begin, end, color) {
+        canvas.beginPath();
+        canvas.moveTo(begin.x, begin.y);
+        canvas.lineTo(end.x, end.y);
+        canvas.lineWidth = 2;
+        canvas.strokeStyle = color;
+        canvas.stroke();
+    }
+
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function (stream) {
+        video.srcObject = stream;
+        video.setAttribute("playsinline", true); // tell iOS safari we don't want fullscreen
+        video.play();
+
+        frame = requestAnimationFrame(tick);
+    });
+
+    function tick() {
+
+        loadingMessage.innerText = "カメラを起動しています..."
+
+        if (video.readyState === video.HAVE_ENOUGH_DATA) {
+
+            btnScan.style.display = "none";
+
+            loadingMessage.hidden = true;
+            canvasElement.hidden = false;
+            outputContainer.hidden = false;
+
+            canvasElement.height = video.videoHeight;
+            canvasElement.width = video.videoWidth;
+            canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+            var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+            var code = jsQR(imageData.data, imageData.width, imageData.height, {
+                inversionAttempts: "dontInvert",
+            });
+
+            if (code && code.data.length > 0) {
+                drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
+                drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
+                drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
+                drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
+                outputMessage.hidden = true;
+                outputData.parentElement.hidden = false;
+                outputData.innerText = code.data;
+
+                video.pause();
+                btnScan.style.display = "inline-block";
+
+                cancelAnimationFrame(frame);
+
+                return;
+
+            } else {
+                outputMessage.hidden = false;
+                outputMessage.innerHTML = 'スキャニング...';
+                outputData.parentElement.hidden = true;
+            }
+        }
+
+        requestAnimationFrame(tick);
+    }
+};
